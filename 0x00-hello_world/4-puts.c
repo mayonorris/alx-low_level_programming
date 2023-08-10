5fc8c9081c9633967034c6b3c985c5b52a1f34b8
Mayo <kadanganorris@gmail.com>
2023-08-10 15:00:59 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main : Program using puts function in C to print a message
- * return : 0 program has terminated successfully.
+ * main - Program using puts function in C to print a message
+ * Return: 0 (program has terminated successfully.)
  */
 int main(void)
 {
