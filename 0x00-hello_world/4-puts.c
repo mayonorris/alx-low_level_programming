d4652070e7b32c4a4eca99f24f5fbe2196f74377
Mayo <kadanganorris@gmail.com>
2023-08-10 14:54:33 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ * main : Program using puts function in C to print a message
+ * return : 0 program has terminated successfully.
+ */
 int main(void)
-{puts("Programming is like  building a multilingual puzzle");
-return (0); 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
