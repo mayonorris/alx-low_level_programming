0605b7eb5c91f99ed901f13814e6a2600b06fdc1
Mayo <kadanganorris@gmail.com>
2023-08-10 16:05:33 +0000

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This program prints variables types
+ * Return: 0 (ended successfully)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
+
