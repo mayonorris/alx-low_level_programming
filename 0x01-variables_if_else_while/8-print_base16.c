927e3c774d38107fae1ee89eb99f6faf0d70393a
Mayo <kadanganorris@gmail.com>
2023-08-12 00:41:12 +0000

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	char ch_hexa;
 
-	for (ch = '0'; ch <= '9'; ch++)
-		putchar(ch);
+	for (ch_hexa = '0'; ch_hexa <= '9'; ch_hexa++)
+		putchar(ch_hexa);
 
-	for (ch = 'a'; ch <= 'f'; ch++)
-		putchar(ch);
+	for (ch_hexa = 'a'; ch_hexa <= 'f'; ch_hexa++)
+		putchar(ch_hexa);
 
 	putchar('\n');
 
