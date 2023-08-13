11caf0cae7b063435ed64041b8bda43d69433839
Mayo <kadanganorris@gmail.com>
2023-08-13 21:58:38 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int length = strlen(message);
-write(2, message "\n", length);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
