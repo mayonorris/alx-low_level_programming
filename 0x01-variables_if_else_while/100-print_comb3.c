7e05069816e1be8c6cf617c3287299cd5b3885fe
Mayo <kadanganorris@gmail.com>
2023-08-13 00:19:31 +0000

    All combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,17 @@
 int main(void)
 {
 	int i = 0;
-	int j = i+1;
+	int j;
 
-	while (i < 10)
-		while (j < 9)
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-			j++;
+	while (i < 9)
+		j = i + 1;
+
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(',');
+		putchar(' ');
 		i++;
+
 	putchar('\n');
 
 	return (0);
