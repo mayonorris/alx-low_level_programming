89f3bc283d6d731bad2bdd7eb50cb465c72952ce
Mayo <kadanganorris@gmail.com>
2023-08-15 23:25:10 +0100

    alphabet with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - A program that prints alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+/* Declare a character variable to contains alphabet*/
+	char ch;
+/* iterate through all the letters in the alphabet, from 'a' to 'z' */
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+  /*print a new line to the console.*/
+	_putchar('\n');
+	return (0);
+}
