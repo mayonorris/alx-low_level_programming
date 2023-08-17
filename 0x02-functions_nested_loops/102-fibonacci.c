47ed1aafa90abfa16df9fc0e5d8b4c99bb2818d2
Mayo <kadanganorris@gmail.com>
2023-08-17 03:54:01 +0100

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,10 +7,11 @@ int main(void)
 {
 	int count = 50;
 	long  fib1 = 1, fib2 = 2;
+	long next = fib1 + fib2;
+	int i;
 
 	for (int i = 0; i < count - 1; i++)
 	{	printf("%lld, ", fib1);
-		long next = fib1 + fib2;
 
 		fib1 = fib2;
 		fib2 = next;
