159d834119648adabfb5e783979c49ba9323a7cf
Mayo <kadanganorris@gmail.com>
2023-08-17 01:51:12 +0100

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints first 50 Fibonacci numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 1;
+	int max = 50;
+	long a = 1;
+	long b = 2;
+
+	while (n <= max / 2)
+	{
+		printf("%li, %li, ", a, b);
+		a += b;
+		b += a;
+		n++;
+	}
+	if (max % 2 == 1)
+		printf("%li", a);
+
+	printf("\n");
+
+	return (0);
+}
