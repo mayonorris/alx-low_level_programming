7f1af1df687f45712cc05da95b3a710a8ef5c374
Mayo <kadanganorris@gmail.com>
2023-09-10 12:41:57 +0100

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,24 +12,31 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, numCoins, count, cents;
+	int coins[5];
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int coins[] = {25, 10, 5, 2, 1};
-	int numCoins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
+	coins[0] = 25;
+	coins[1] = 10;
+	coins[2] = 5;
+	coins[3] = 2;
+	coins[4] = 1;
+	numCoins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
 
-	for (int i = 0; i < numCoins; i++)
+	for (i = 0; i < numCoins; i++)
 	{
 		while (cents >= coins[i])
 		{
