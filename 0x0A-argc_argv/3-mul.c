b791094fe67a9533ba93799c6a03e19b31c99e2b
Mayo <kadanganorris@gmail.com>
2023-09-10 03:10:53 +0100

    mutiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[])
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
-	int result = num1 * num2;
+	int result 
+
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
