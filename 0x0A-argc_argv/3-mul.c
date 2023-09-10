6c25c34741b9a7ba79311a95091becbae691d64d
Mayo <kadanganorris@gmail.com>
2023-09-10 03:35:45 +0100

    multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
-	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
 
