de251d4945576a550f46ba97dd0e6fbeaddf7c5a
Mayo <kadanganorris@gmail.com>
2023-09-12 02:00:50 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: Name of the dog
+ * @age: Age
+ * @owner: The owner of the dog
+ * Description: Main propreties of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
