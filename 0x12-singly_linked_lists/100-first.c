4c7729f3d840f4f75dece7064febd879368f186c
Mayo <kadanganorris@gmail.com>
2023-09-23 02:51:58 +0100

    pre main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  */
 void pre_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\\nI");
+	printf("bore my house upon my back!\\n \n");
 }
 
