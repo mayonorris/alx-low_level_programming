65160d3342c0ff8b832cc92350de91977dfebd40
Mayo <kadanganorris@gmail.com>
2023-09-23 02:44:16 +0100

    pre main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * pre_main - Prints a message before the main function is executed.
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
