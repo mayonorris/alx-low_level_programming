b30cd81299076b3c18bdf8598d1abc4ffc55880c
Mayo <kadanganorris@gmail.com>
2023-09-23 02:56:09 +0100

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    hello db 'Hello, Holberton',0
+
+section .text
+    global _start
+
+_start:
+    ; write the string to stdout
+    mov eax, 0x4          ; syscall number for sys_write
+    mov edi, 0x1          ; file descriptor 1 (stdout)
+    mov edx, 0x11         ; length of the string
+    lea rsi, [hello]      ; address of the string
+    syscall
+
+    ; exit the program
+    mov eax, 0x60         ; syscall number for sys_exit
+    xor edi, edi          ; exit code 0
+    syscall
+
