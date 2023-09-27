24813401ea04788ee3a2f3f621e72da54e6522d8
Mayo <kadanganorris@gmail.com>
2023-09-27 19:37:59 +0100

    Find the loop in a linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,33 +1,41 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
 
+void free_list(listint_t *head);
+
 /**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: A pointer to a pointer to the head of the list.
- * Return: The size of the list that was freed.
+ * free_listint_safe - A function that frees a list
+ * @h: A pointer listint_t structure
+ * Return: The size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *temp, *current;
+    size_t counter = 0;
+    listint_t *temp;
 
-	if (!h || !*h)
-		return (count);
+    while (*h)
+    {
+        temp = *h;
+        *h = (*h)->next;
+        free(temp);
+        counter++;
+    }
 
-	current = *h;
-	*h = NULL;
+    return (counter);
+}
 
-	while (current)
-	{
-		count++;
-		temp = current;
-		current = current->next;
-		free(temp);
-
-		if (temp <= current)
-			break;
-	}
-
-	return (count);
+/**
+ * free_list - A function that frees a listint_t recursively
+ * @head: A pointer to the listint_t structure
+ * Return: Nothing
+ */
+void free_list(listint_t *head)
+{
+    if (head)
+    {
+        free_list(head->next);
+        free(head);
+    }
 }
 
