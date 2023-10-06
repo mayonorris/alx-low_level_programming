15b95a09253ac3b3141518c2a7e6115f83ef6759
Mayo <kadanganorris@gmail.com>
2023-10-07 00:59:53 +0100

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
 #ifndef _FILE_IO_H
 #define _FILE_IO_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 void _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
