5a7f1139ace8c6912ad66c88d666eac487a3180a
Mayo <kadanganorris@gmail.com>
2024-02-22 03:13:01 +0100

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to calculate the perimeter of an island in a grid"""
 
+
 def island_perimeter(grid):
     """Calculates the perimeter of the island described in the grid
 
